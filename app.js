97fe2981bbb63222071757eb4d5d0f27c1883bb5
studioshane <studioshane@gmail.com>
2019-05-21 22:16:12 +0100

    gertting input from command line with yargs

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 getNotes = require('./notes');
 
-const notes = getNotes();
+yargs.version('1.0.1');
 
-console.log(notes);
+yargs.command({
+	command: 'add',
+	description: 'Add a note',
+	builder: {
+		title: { describe: 'Note Title', demand: true, type: 'string' },
+		body: { describe: 'Note Body', demand: true, type: 'string' }
+	},
+	handler: argv => {
+		console.log('Title: ' + argv.title);
+		console.log('Note: ' + argv.body);
+	}
+});
 
-// add = require('./utils');
-// const sum = add(144, 1);
-// console.log(sum);
+yargs.command({
+	command: 'remove',
+	description: 'Remove a note',
+	handler: () => console.log('removing note')
+});
+yargs.command({
+	command: 'list',
+	description: 'List all notes',
+	handler: () => console.log('listing the notes')
+});
+yargs.command({
+	command: 'read',
+	description: 'Read a note',
+	handler: () => console.log('reading a note')
+});
+
+// console.log(yargs.argv);
+
+yargs.parse();
